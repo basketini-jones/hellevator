f96d141717fcef848a80d6bfa5a58fdbf924701f
basketini-jones <60999059+basketini-jones@users.noreply.github.com>
2021-10-27 11:37:02 -0400

    add slashing

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,6 +11,14 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float jumpBufferLength;
     private float jumpBufferCount;
 
+    [SerializeField] private float slashSpeed;
+    [SerializeField] private float slashTime;
+    [SerializeField] private float slashAmount;
+    private float currentSlashAmount;
+    private float slashTimeCount;
+    private Vector2 slashDirection;
+    private string SLASH_STATE = "IDLE";
+
     private Rigidbody2D body;
     private SpriteRenderer sprite;
     private Animator animator;
@@ -20,46 +28,89 @@ public class PlayerMovement : MonoBehaviour
         body = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         sprite = GetComponent<SpriteRenderer>();
+        currentSlashAmount = slashAmount;
     }
 
     private void Update()
     {
-        // horizontal movement
-        int left = Input.GetKey(KeyCode.A) ? 1 : 0;
-        int right = Input.GetKey(KeyCode.D) ? 1 : 0;
-        int direction = right - left;
-
-        body.velocity = new Vector2(walkSpeed * direction, body.velocity.y);
-        sprite.flipX = body.velocity.x < 0;
-        animator.SetFloat("Speed", Mathf.Abs(body.velocity.x));
-
-        //Jump buffer
-        if (Input.GetButtonDown("Jump"))
+        if (SLASH_STATE != "SLASHING")
         {
-            jumpBufferCount = jumpBufferLength;
-        }
-        else
-        {
-            jumpBufferCount -= Time.deltaTime;
-        }
+            // horizontal movement
+            int left = Input.GetKey(KeyCode.A) ? 1 : 0;
+            int right = Input.GetKey(KeyCode.D) ? 1 : 0;
+            int direction = right - left;
 
-        //Handle jump inputs
-        if (jumpBufferCount >= 0 && grounded)
-        {
-            body.velocity = new Vector2(body.velocity.x, maxJumpSpeed);
-            jumpBufferCount = 0;
-        }
+            body.velocity = new Vector2(walkSpeed * direction, body.velocity.y);
+            sprite.flipX = body.velocity.x < 0;
+            animator.SetFloat("Speed", Mathf.Abs(body.velocity.x));
 
-        if(Input.GetButtonUp("Jump") && body.velocity.y > minJumpSpeed)
-        {
-            body.velocity = new Vector2(body.velocity.x, minJumpSpeed);
-        }
+            //Jump buffer
+            if (Input.GetButtonDown("Jump"))
+            {
+                jumpBufferCount = jumpBufferLength;
+            }
+            else
+            {
+                jumpBufferCount -= Time.deltaTime;
+            }
 
-        if (grounded)
-            animator.SetBool("IsJumping", false);
-        else
-            animator.SetBool("IsJumping", true);
-}
+            //Handle jump inputs
+            if (jumpBufferCount >= 0 && grounded)
+            {
+                body.velocity = new Vector2(body.velocity.x, maxJumpSpeed);
+                jumpBufferCount = 0;
+            }
+
+            if (Input.GetButtonUp("Jump") && body.velocity.y > minJumpSpeed)
+            {
+                body.velocity = new Vector2(body.velocity.x, minJumpSpeed);
+            }
+
+            if (grounded)
+                animator.SetBool("IsJumping", false);
+            else
+                animator.SetBool("IsJumping", true);
+        }
+        if (SLASH_STATE == "SLASHING")
+        {
+            body.velocity = Vector2.zero;
+            Slash();
+            slashTimeCount += Time.deltaTime;
+            if (slashTimeCount >= slashTime)
+            {
+                if (currentSlashAmount > 0)
+                    body.gravityScale = 2.5f;
+                else
+                    body.gravityScale = 5f;
+                SLASH_STATE = "FALL";
+            }
+        }
+        if (SLASH_STATE == "FALL")
+        {
+            if (grounded)
+            {
+                currentSlashAmount = slashAmount;
+                body.gravityScale = 5;
+                SLASH_STATE = "IDLE";
+            }
+        }
+        //Slashing
+        if (Input.GetMouseButtonDown(0) && SLASH_STATE != "SLASHING" && currentSlashAmount > 0)
+        {
+            Vector2 target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 angle = target - new Vector2(transform.position.x, transform.position.y);
+            slashDirection = angle.normalized;
+            body.gravityScale = 0;
+            slashTimeCount = 0;
+            currentSlashAmount--;
+            SLASH_STATE = "SLASHING";
+        }
+    }
+
+    private void Slash()
+    {
+        transform.position = new Vector2(transform.position.x, transform.position.y) + slashDirection * slashSpeed * Time.deltaTime;
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
