4e7e22190fc095642df0e5b882917494c20061b6
Ryan Dixon <rjd323@drexel.edu>
2021-10-28 13:59:52 -0400

    add pixel perfect cam at 320x180

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -19,7 +19,9 @@ public class PlayerMovement : MonoBehaviour
     private float slashTimeCount;
     private float groundSlashBufferCount;
     private Vector2 slashDirection;
-    private string SLASH_STATE = "IDLE";
+
+    private string STATE; //POTENTIAL STATE: "IDLE", "SLASH", "FALL"
+    private bool controlsEnabled;
 
     [SerializeField] private GameObject aimLine;
     private Rigidbody2D body;
@@ -32,11 +34,43 @@ public class PlayerMovement : MonoBehaviour
         animator = GetComponent<Animator>();
         sprite = GetComponent<SpriteRenderer>();
         currentSlashAmount = slashAmount;
+        STATE = "IDLE";
     }
 
     private void Update()
     {
-        if (SLASH_STATE != "SLASH")
+        if (STATE == "IDLE")
+        {
+            controlsEnabled = true;
+            RenderAimline(true);
+        }
+
+        if (STATE == "SLASH")
+        {
+            controlsEnabled = false;
+            RenderAimline(false);
+            Slash();
+            slashTimeCount += Time.deltaTime;
+            if (slashTimeCount >= slashTime)
+            {
+                animator.SetBool("IsSlashing", false);
+                slashDirection = Vector2.zero;
+                STATE = "FALL";
+            }
+        }
+
+        if (STATE == "FALL")
+        {
+            controlsEnabled = true;
+            RenderAimline(currentSlashAmount > 0);
+            if (grounded)
+            {
+                currentSlashAmount = slashAmount;
+                STATE = "IDLE";
+            }
+        }
+
+        if (controlsEnabled)
         {
             // horizontal movement
             int left = Input.GetKey(KeyCode.A) ? 1 : 0;
@@ -69,60 +103,55 @@ public class PlayerMovement : MonoBehaviour
                 body.velocity = new Vector2(body.velocity.x, minJumpSpeed);
             }
 
-            if (grounded || SLASH_STATE == "SLASH")
+            if (grounded || STATE == "SLASH")
                 animator.SetBool("IsJumping", false);
             else
                 animator.SetBool("IsJumping", true);
         }
-        if (SLASH_STATE == "SLASH")
-        {
-            animator.SetBool("IsSlashing", true);
-            body.velocity = Vector2.zero;
-            Slash();
-            slashTimeCount += Time.deltaTime;
-            if (slashTimeCount >= slashTime)
-            {
-                if (currentSlashAmount > 0)
-                    body.gravityScale = 2.5f;
-                else
-                    body.gravityScale = 5f;
-                animator.SetBool("IsSlashing", false);
-                slashDirection = Vector2.zero;
-                SLASH_STATE = "FALL";
-            }
-        }
-        if (SLASH_STATE == "FALL")
-        {
-            if (grounded)
-            {
-                currentSlashAmount = slashAmount;
-                body.gravityScale = 5;
-                SLASH_STATE = "IDLE";
-            }
-        }
         //Slashing
         if (Input.GetMouseButtonDown(0))
-        {
             groundSlashBufferCount = groundSlashBufferLength;
-        }
         else
-        {
             groundSlashBufferCount -= Time.deltaTime;
-        }
 
-        if (groundSlashBufferCount >= 0 && SLASH_STATE != "SLASH" && currentSlashAmount > 0)
+        if (groundSlashBufferCount >= 0 && STATE != "SLASH" && currentSlashAmount > 0)
         {
             Vector2 target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Vector2 slashAngle = target - new Vector2(transform.position.x, transform.position.y);
             slashDirection = slashAngle.normalized;
-            body.gravityScale = 0;
             slashTimeCount = 0;
             currentSlashAmount--;
-            SLASH_STATE = "SLASH";
+            RenderAimline(true);
+            STATE = "SLASH";
         }
 
-        //Aimline rotation
-        if (SLASH_STATE != "SLASH" && currentSlashAmount > 0)
+        //Sprite flipping
+        sprite.flipX = body.velocity.x < 0 || slashDirection.x * slashSpeed < 0;
+
+        ManageGravity();
+
+    }
+
+    private void Slash()
+    {
+        animator.SetBool("IsSlashing", true);
+        body.velocity = Vector2.zero;
+        transform.position = new Vector2(transform.position.x, transform.position.y) + slashDirection * slashSpeed * Time.deltaTime;
+    }
+
+    private void ManageGravity()
+    {
+        if (STATE == "SLASH")
+            body.gravityScale = 0f;
+        else if (STATE == "FALL" && currentSlashAmount > 0)
+            body.gravityScale = 2.5f;
+        else
+            body.gravityScale = 5f;
+    }
+
+    private void RenderAimline(bool enable)
+    {
+        if (enable)
         {
             aimLine.SetActive(true);
             Vector2 aimVector = Camera.main.WorldToScreenPoint(transform.position);
@@ -134,18 +163,7 @@ public class PlayerMovement : MonoBehaviour
             aimLine.transform.rotation = Quaternion.AngleAxis(aimAngle, Vector3.forward);
         }
         else
-        {
             aimLine.SetActive(false);
-        }
-
-        //Sprite flipping
-        sprite.flipX = body.velocity.x < 0 || slashDirection.x * slashSpeed < 0;
-
-    }
-
-    private void Slash()
-    {
-        transform.position = new Vector2(transform.position.x, transform.position.y) + slashDirection * slashSpeed * Time.deltaTime;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
